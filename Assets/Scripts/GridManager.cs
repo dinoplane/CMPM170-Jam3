e2f783f68b81653a90dc9c8cabd9862480600eaf
dinoplane <isacsac2017@gmail.com>
2022-11-18 12:12:06 -0800

    isometric grids are weird...

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+
+public class GridManager : MonoBehaviour
+{
+    private GameObject cursor;
+    private Grid tmap;
+
+    private Vector3Int tileCoords;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursor = GameObject.Find("Cursor");
+        tmap = GameObject.Find("Grid").GetComponent<Grid>();
+        tileCoords = new Vector3Int(0, -1, 0);
+
+        setCursorPos();
+    }
+
+    public void OnGridMovement(InputAction.CallbackContext context){
+
+        //Debug.Log($"Movement {context.phase} {context.ReadValue<Vector2>()}");
+        Vector2Int curr_val = Vector2Int.CeilToInt(context.ReadValue<Vector2>());
+
+        
+        
+        switch (context.phase){
+            case InputActionPhase.Started:
+                break;
+            
+            case InputActionPhase.Performed:
+                Vector3Int newval = new Vector3Int();
+                newval.x = +curr_val.x + curr_val.y;
+                newval.y = -curr_val.x + curr_val.y;
+                Debug.Log(newval);
+                updateCursorPos(newval);
+                break;
+            
+            case InputActionPhase.Canceled:
+
+                break;
+        }
+    }
+
+    private void updateCursorPos(Vector3Int vIn){
+        //tileCoords
+        tileCoords += vIn;
+        setCursorPos();
+    }
+    
+    private void setCursorPos(){
+        Vector3 dest = tmap.GetCellCenterLocal(tileCoords);
+        dest.z = 0;
+        cursor.transform.position = dest;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
