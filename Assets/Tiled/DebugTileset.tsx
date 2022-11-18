5ef82ac8b1c9654eed038d89fb874d0f659664ae
dinoplane <isacsac2017@gmail.com>
2022-11-18 10:05:58 -0800

    Init and Input

---
+++ Assets/Tiled/DebugTileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="DebugTileset" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <grid orientation="isometric" width="64" height="64"/>
+ <image source="DebugTileset.png" width="128" height="128"/>
+</tileset>
